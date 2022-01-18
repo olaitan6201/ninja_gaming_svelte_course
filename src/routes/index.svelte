8f039483eb606d29f712b30e4e582102f145ee43
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-01-18 01:10:57 +0100

    Completed component refresh

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'Ninja Gaming Guides';
+
+    const updateTitle = () => {
+        title = "Some Other Title Content";
+    }
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam debitis culpa harum excepturi nesciunt ea iste fugiat corrupti error incidunt fuga temporibus recusandae sapiente nisi, quia adipisci, ducimus est neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus tempore recusandae porro debitis atque, voluptates doloremque deserunt sequi enim odio veritatis nulla minus eos aut rem harum, dolores quod dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vero magnam accusamus quia, suscipit iusto voluptatem quidem illo voluptate asperiores, tempore eligendi mollitia magni non nobis corrupti quis unde nostrum.</p>
+
+    <input type="text" bind:value={title}>
+    <button on:click={updateTitle}>Change Title</button>
+</div>
+
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
