578ec93aa36f65343f1e72994ea55df1e877badc
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-01-18 01:21:19 +0100

    Completed Pages & Components

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<div class="about">
+    <h2>About Us</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus, eos obcaecati suscipit veritatis, officia nihil et libero voluptates provident hic sed quia. Veritatis placeat consequatur dolorum adipisci sapiente recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe temporibus quibusdam consequatur? Natus, in obcaecati numquam aliquid labore dolores omnis impedit ex? Non quam doloremque ut deserunt amet vitae consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos ab quas ex aut! Quas, voluptatum adipisci. In doloribus distinctio obcaecati quod eum voluptatum laudantium, laborum, non officia odit odio.</p>
+
+    
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,13 @@
 <script>
-    let title = 'Ninja Gaming Guides';
 
-    const updateTitle = () => {
-        title = "Some Other Title Content";
-    }
 </script>
 
 <div class="index">
-    <h2>{title}</h2>
+    <h2>Welcome</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam debitis culpa harum excepturi nesciunt ea iste fugiat corrupti error incidunt fuga temporibus recusandae sapiente nisi, quia adipisci, ducimus est neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus tempore recusandae porro debitis atque, voluptates doloremque deserunt sequi enim odio veritatis nulla minus eos aut rem harum, dolores quod dolorum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed vero magnam accusamus quia, suscipit iusto voluptatem quidem illo voluptate asperiores, tempore eligendi mollitia magni non nobis corrupti quis unde nostrum.</p>
 
-    <input type="text" bind:value={title}>
-    <button on:click={updateTitle}>Change Title</button>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
 </div>
 
 <style>
